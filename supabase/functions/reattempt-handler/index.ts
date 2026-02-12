c8c5903964f29f4cbdf0ce1529794ef33e0218e4
Mohammed Ajooz <m.ajooz@galinaexpress.com>
2026-02-12 13:23:07 +0300

    step 2

---
+++ supabase/functions/reattempt-handler/index.ts
@@ -0,0 +1,8 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+
+serve(async (req) => {
+  return new Response(
+    JSON.stringify({ message: "Reattempt handler working 🚀" }),
+    { headers: { "Content-Type": "application/json" } },
+  );
+});
